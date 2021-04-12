6e0bd10e21afce842ed3ea37c121686bebf30ad8
Suman <sumanj631@gmail.com>
2021-04-12 18:25:28 +0530

    init

---
+++ test.js
@@ -0,0 +1,113 @@
+
+function one()
+{
+  document.getElementById("p").style.visibility="hidden";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="hidden";
+  document.getElementById("b").style.visibility="hidden";
+  document.getElementById("c").style.visibility="hidden";
+
+}
+function two()
+{
+  document.getElementById("p").style.visibility="hidden";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="hidden";
+  document.getElementById("s").style.visibility="";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function three()
+{
+  document.getElementById("p").style.visibility="hidden";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function four()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="hidden";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="hidden";
+}
+function five()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="hidden";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function six()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="hidden";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function seven()
+{
+  document.getElementById("p").style.visibility="hidden";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="hidden";
+  document.getElementById("c").style.visibility="hidden";
+}
+function eight()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function nine()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="hidden";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="";
+  document.getElementById("c").style.visibility="";
+}
+function zero()
+{
+  document.getElementById("p").style.visibility="";
+  document.getElementById("q").style.visibility="";
+  document.getElementById("r").style.visibility="";
+  document.getElementById("s").style.visibility="";
+  document.getElementById("a").style.visibility="";
+  document.getElementById("b").style.visibility="hidden";
+  document.getElementById("c").style.visibility="";
+}
+setTimeout(zero,0);
+setTimeout(one,1000);
+setTimeout(two,2000);
+setTimeout(three,3000);
+setTimeout(four,4000);
+setTimeout(five,5000);
+setTimeout(six,6000);
+setTimeout(seven,7000);
+setTimeout(eight,8000);
+setTimeout(nine,9000);
+
